9a6b91ce78c4b3aca4ebf129f76c0ff6202903fb
fhamood <firas.hamood@tum.de>
2024-11-14 18:04:05 +0100

    picked_fdr_functions.py cleanup + docstrings

--- prosimsit/picked_fdr_functions.py
+++ prosimsit/picked_fdr_functions.py
@@ -8,20 +8,28 @@ from picked_group_fdr.pipeline import update_evidence_from_pout
 logger = logging.getLogger(__package__ + "." + __file__)
 
 def run_picked_protein_group_fdr(percolator_dir, picked_dir, fasta, enzyme):
-    def add_extra_dashes_for_percolator(path_to_file, path_to_file_out):
+    """
+    Run the Picked Protein Group FDR pipeline
+    :param percolator_dir: Path to the percolator output directory
+    :param picked_dir: Path to the picked protein group FDR output directory
+    :param fasta: Path to the fasta file used for database search with MaxQuant
+    :param enzyme: Enzyme used for database search with MaxQuant; usually 'trypsin' or 'trpysinp'
+    :return: None
+    """
+    def add_extra_dash_for_percolator(path_to_file_in, path_to_file_out):
         """
-        deletes extra dashes from the epercolator output
-        :param path_to_file:
-        :param path_to_file_out:
+        Add extra dashes to the PSMId column of the percolator input file
+        :param path_to_file_in: path to the percolator input file
+        :param path_to_file_out: path to the output file with extra dash
         :return:
         """
-        df = pd.read_csv(path_to_file, sep='\t')
+        df = pd.read_csv(path_to_file_in, sep='\t')
         df['PSMId'] += '-1'
         df.to_csv(path_to_file_out, sep='\t', index=False)
 
-    add_extra_dashes_for_percolator(f'{percolator_dir}/rescore_all.percolator.psms.txt',
+    add_extra_dash_for_percolator(f'{percolator_dir}/rescore_all.percolator.psms.txt',
                                     f'{percolator_dir}/rescore_all.percolator.psms.dash.txt')
-    add_extra_dashes_for_percolator(f'{percolator_dir}/rescore_all.percolator.decoy.psms.txt',
+    add_extra_dash_for_percolator(f'{percolator_dir}/rescore_all.percolator.decoy.psms.txt',
                                     f'{percolator_dir}/rescore_all.percolator.decoy.psms.dash.txt')
 
     update_evidence_from_pout.main([
