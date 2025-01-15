5036f435b931c70278bcf9c6dc67c5063be0b194
fhamood <firas.hamood@tum.de>
2025-01-15 01:08:30 +0100

    Test blanks for simsi_functions.py and io.py

---
+++ tests/unittests/test_io.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+class TestReadMsmsSinglecol:
+    @pytest.fixture
+    def example_dataframes(self):
+        pass
+
+    def test_merge_with_msmstxt(self, example_dataframes):
+        assert True == True
---
+++ tests/unittests/test_simsi_functions.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+class TestPrepareSimsiFiles:
+    @pytest.fixture
+    def example_dataframes(self):
+        pass
+
+    def test_prepare_simsi_files(self, example_dataframes):
+        assert True == True
+
+
+class TestBuildEvidence:
+    @pytest.fixture
+    def example_dataframes(self):
+        pass
+
+    def test_build_evidence(self, example_dataframes):
+        assert True == True
